a09cfa5aeab94b39e2a6f12815600b8d251fabd4
Charles Müller <charles.mueller@outlook.com>
2025-09-06 15:27:02 +0200

    Upgrade to 1.21.8 and include lead recipe and removal of saddle recipe

--- src/main/java/com/syntaxcacao/spigot/RabbitHideBundlePlugin.java
+++ src/main/java/com/syntaxcacao/spigot/RabbitHideBundlePlugin.java
@@ -6,20 +6,41 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/** @noinspection unused  */
 public class RabbitHideBundlePlugin extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
+        // BUNDLE //
+
         // Remove default bundle recipe
         this.getServer().removeRecipe(NamespacedKey.minecraft("bundle"));
 
         // Add new bundle recipe
-        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "bundle"), new ItemStack(Material.BUNDLE));
-        recipe.shape("SHS", "H H", "HHH");
-        recipe.setIngredient('H', Material.RABBIT_HIDE);
-        recipe.setIngredient('S', Material.STRING);
+        ShapedRecipe bundleRecipe = new ShapedRecipe(new NamespacedKey(this, "bundle"), new ItemStack(Material.BUNDLE));
+        bundleRecipe.shape("SHS", "H H", "HHH");
+        bundleRecipe.setIngredient('H', Material.RABBIT_HIDE);
+        bundleRecipe.setIngredient('S', Material.STRING);
 
-        this.getServer().addRecipe(recipe);
+        this.getServer().addRecipe(bundleRecipe);
+
+        // LEAD //
+
+        // Remove default lead recipe
+        this.getServer().removeRecipe(NamespacedKey.minecraft("lead"));
+
+        // Add new lead recipe
+        ShapedRecipe leadRecipe = new ShapedRecipe(new NamespacedKey(this, "lead"), new ItemStack(Material.LEAD, 2));
+        leadRecipe.shape(" SS", " BS", "S  ");
+        leadRecipe.setIngredient('S', Material.STRING);
+        leadRecipe.setIngredient('B', Material.SLIME_BALL);
+
+        this.getServer().addRecipe(leadRecipe);
+
+        // SADDLE //
+
+        // Remove saddle recipe
+        this.getServer().removeRecipe(NamespacedKey.minecraft("saddle"));
     }
 }
